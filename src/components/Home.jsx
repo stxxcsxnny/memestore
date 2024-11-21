87939900106b7e521a7686bd1d83579fd916bc0b
sunny <sk2993130@gmail.com>
2024-11-21 11:29:41 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
       <>
           <div className='home' id='home'>
           <main>
-              <h1>SUNNY</h1>
-              <p>solution to all your problems</p>
+              <h1>MEMESTORE</h1>
+              <p>Memes for all your problems</p>
          </main>
          
           </div>
@@ -16,7 +16,7 @@ const Home = () => {
               <img src={vg} alt='graphic' />
               <div>
                   <p>
-                  "Facing a challenge? We’ve got you covered! Our team of experts provides customized solutions for any problem, big or small. With 24/7 support and innovative strategies, we turn obstacles into opportunities, ensuring you achieve the results you need. Your solution is just a step away!"
+                  "Got a problem? We've got the meme! Our page serves up hilarious, relatable memes for every situation. From daily struggles to unexpected mishaps, we're here to make you laugh through it all. Join us and turn every problem into a punchline!"
                   </p>
               </div>
           </div>
@@ -26,7 +26,7 @@ const Home = () => {
                   who we are?
                   </h1>
                   <p>
-                  Welcome to [Your Meme Page Name]  your ultimate destination for daily laughs, relatable content, and meme magic! We’re a community of humor enthusiasts who believe in the power of a good meme to brighten your day, share a laugh, and bring people together.
+                  Welcome to Memestore  your ultimate destination for daily laughs, relatable content, and meme magic! We’re a community of humor enthusiasts who believe in the power of a good meme to brighten your day, share a laugh, and bring people together.
 
 Our mission? To spread joy, one meme at a time. From classic memes to trending topics, we’ve got it all. So, whether you're a meme veteran or new to the scene, join us in celebrating the universal language of humor. Because life’s too short not to laugh!
 
@@ -37,7 +37,7 @@ Stay connected, stay entertained, and let’s make the world a funnier place tog
           </div>
           <div className="home4" id='brands'>
               <div>
-                  <h1>memes</h1>
+                  <h1>Memes</h1>
 
                   <article>
                       <div style={{animationDelay: '0.3s'}}>
